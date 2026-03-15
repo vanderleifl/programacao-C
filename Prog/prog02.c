09fa7995e9bf8a32c10f7aeeb5cb035e3cf7ea8f
Vanderlei <vanderleifl@hotmail.com>
2026-03-15 13:27:46 -0300

    Curso ADS

--- Prog/prog02.c
+++ Prog/prog02.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
     char nome[50];
     int idade;
 
-    printf("Digite seu nome: ");
-    scanf("%s", nome);
+    printf("\nDigite seu nome: ");
+    fgets(nome, sizeof(nome), stdin);
+    nome[strcspn(nome, "\n")] = 0; // Remove o caractere de nova linha
     printf("Digite sua idade: ");
     scanf("%d", &idade);
-    printf("\n\nOlá, %s! Você tem %d anos.\n", nome, idade);
+    printf("\nOlá, %s! Você tem %d anos.\n\n", nome, idade);
     return 0;
 }
